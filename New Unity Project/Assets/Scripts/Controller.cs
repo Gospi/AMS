5727317797239545620d8096b6f67e8b0be5f0f5
Kin Liu <kin.liu@hotmail.com>
2017-12-11 18:56:23 +0100

    Add sensor and basic controller

---
+++ New Unity Project/Assets/Scripts/Controller.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    private List<KeyValuePair<float, float>> laserReadings = new List<KeyValuePair<float, float>>();
+    private List<Vector3> visitedPositions = new List<Vector3>();
+    private Vector3 nextDestination;
+    private MovementState movementState;
+    private int maxDegrees = 360;
+    private int degressRotated = 0;
+    private float radius = 0.5f;
+    private bool moved = false;
+    public Sensor mainSensor;
+
+    public enum MovementState
+    {
+        Stop, Prepare, Moving
+    }
+
+	// Use this for initialization
+	void Start () {
+        this.visitedPositions.Add(this.transform.position);
+        this.movementState = MovementState.Stop;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.HandleMovement(this.movementState);
+	}
+
+    private void HandleMovement(MovementState movementState)
+    {
+        switch (movementState)
+        {
+            case MovementState.Stop:
+                if (degressRotated < maxDegrees)
+                {
+                    var distance = this.mainSensor.MeasureDistance();
+                    this.mainSensor.transform.Rotate(this.mainSensor.transform.up, 1.0f);
+                    this.laserReadings.Add(new KeyValuePair<float, float>(degressRotated, distance));
+                    degressRotated++;
+                } else
+                {
+                    this.movementState = MovementState.Prepare;
+                }
+                break;
+            case MovementState.Prepare:
+                this.laserReadings = this.laserReadings.OrderByDescending(x => x.Value).ToList();
+                KeyValuePair<float, float> reading = new KeyValuePair<float, float>(0, 0);
+                reading = this.laserReadings[0];
+                var nextDestination = this.transform.position + this.transform.forward * reading.Value * 0.2f;
+                this.nextDestination = nextDestination;
+                this.transform.Rotate(this.transform.up, reading.Key);
+                this.nextDestination = this.transform.position + this.transform.forward * reading.Value * 0.2f;
+                this.movementState = MovementState.Moving;
+                break;
+            case MovementState.Moving:
+                this.ResetLasers();
+                this.transform.position = Vector3.MoveTowards(this.transform.position, this.nextDestination, 0.05f);
+                if (this.transform.position == this.nextDestination)
+                {
+                    this.visitedPositions.Add(this.transform.position);
+                    this.degressRotated = 0;
+                    this.nextDestination = this.transform.position;
+                    this.movementState = MovementState.Stop;
+                    this.laserReadings.Clear();
+                }
+                break;
+        }
+    }
+
+    private void ResetLasers()
+    {
+        this.mainSensor.ResetLaser();
+    }
+
+}
---
+++ New Unity Project/Assets/Scripts/Sensor.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensor : MonoBehaviour
+{
+
+    public float MAX_DISTANCE = 10.0f;
+    private const float MIN_DISTANCE = 0.01f;
+    private LineRenderer laserLineRenderer;
+    public float laserWidth = 0.05f;
+
+    public float Output
+    {
+        get;
+        private set;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        laserLineRenderer = this.GetComponent<LineRenderer>();
+        this.ResetLaser();
+        laserLineRenderer.startWidth = laserWidth;
+        laserLineRenderer.endWidth = laserWidth;
+
+    }
+
+    private void DrawLaser(bool hitDetected, RaycastHit hitInfo)
+    {
+        this.laserLineRenderer.SetPosition(0, this.transform.position);
+        if (hitDetected)
+        {
+            this.laserLineRenderer.startColor = Color.red;
+            laserLineRenderer.endColor = Color.red;
+            laserLineRenderer.SetPosition(1, hitInfo.point);
+        }
+        else
+        {
+            laserLineRenderer.startColor = Color.green;
+            laserLineRenderer.endColor = Color.green;
+            laserLineRenderer.SetPosition(1, this.transform.position + this.transform.forward * MAX_DISTANCE);
+        }
+    }
+
+    public float MeasureDistance()
+    {
+        RaycastHit hitInfo;
+        var hitDetected = Physics.Raycast(this.transform.position, this.transform.forward, out hitInfo, MAX_DISTANCE);
+        this.DrawLaser(hitDetected, hitInfo);
+        if(hitDetected)
+        {
+            return Vector3.Distance(this.transform.position, hitInfo.point);
+        } else
+        {
+            return MAX_DISTANCE;
+        }
+    }
+
+    public void ResetLaser()
+    {
+        Vector3[] initLaserPositions = new Vector3[2] { Vector3.zero, Vector3.zero };
+        laserLineRenderer.SetPositions(initLaserPositions);
+    }
+}
